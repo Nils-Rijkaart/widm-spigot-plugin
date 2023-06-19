7d341c7054e5128aad34d78c86c2602f4db0d42a
Nils Rijkaart van Cappellen <nils@nilsrijkaart.nl>
2023-06-20 01:01:01 +0200

    respawn event

--- src/main/kotlin/nl/nilsrijkaart/widm/events/DeathEvent.kt
+++ src/main/kotlin/nl/nilsrijkaart/widm/events/DeathEvent.kt
@@ -6,22 +6,26 @@ import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerRespawnEvent
 
 class DeathEvent : Listener {
     @EventHandler
     fun onDeath(event: PlayerDeathEvent) {
+        if (GameManager.game?.rules?.get(GameRule.DEATH_MESSAGE) != true) {
+            event.deathMessage = null
+            return
+        }
+    }
+
+    @EventHandler
+    fun onRespawn(event: PlayerRespawnEvent) {
         if (GameManager.game != null) {
             val host = GameManager.game!!.hosts.first {
                 it.isOnline
             }
-            event.entity.teleport(host.location)
-            event.entity.gameMode = GameMode.SPECTATOR
-            event.entity.sendMessage("§cJe bent dood gegaan :( Je kunt nu alleen nog maar toekijken. Respecteer hierbij de regels, ga niet verder kamers in dan de game is en kijk niet in kisten.")
-        }
-
-        if (GameManager.game?.rules?.get(GameRule.DEATH_MESSAGE) != true) {
-            event.deathMessage = null
-            return
+            event.player.teleport(host.location)
+            event.player.gameMode = GameMode.SPECTATOR
+            event.player.sendMessage("§cJe bent dood gegaan :( Je kunt nu alleen nog maar toekijken. Respecteer hierbij de regels, ga niet verder kamers in dan de game is en kijk niet in kisten.")
         }
     }
 }
